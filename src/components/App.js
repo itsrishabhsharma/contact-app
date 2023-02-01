988689cb97ef026f0d98a274147d7719b7a8ffd7
Rishabh Sharma <itsrishabhsharma@gmail.comm>
2023-02-02 03:15:20 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import './App.css';
+import Header from "./Header";
+import AddContacts from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* <AddContacts />
+      <ContactList /> */}
+    </div>
+  )
+
+}
+
+export default App;
